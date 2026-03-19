8e0679aa6c578999cf47bf1a09b6e23f393e1f10
buttercup2007 <2215907@talnet.nl>
2026-03-19 10:07:42 +0100

    fade css

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -473,4 +473,11 @@ h1 {
   font-size: 24px;
   margin-top: 100px;
 }
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
 </style>
